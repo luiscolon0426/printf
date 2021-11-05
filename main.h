1e1f172b278e1034ad5dd31140cba1570b0ccf40
luiscolon0426 <luiscolon0426@gmail.com>
2021-11-05 21:10:03 +0000

    header v1

--- main.h
+++ main.h
@@ -4,7 +4,12 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <limits.h>
+
 int _printf(const char *format, ...);
+int printchar(char *s, ...);
+int printdecimal(char *s, ...);
+int printstr(char *s, ...);
+int printint(char *s, ...);
 
 
 
