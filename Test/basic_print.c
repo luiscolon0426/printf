b480ef7e4a0481b0cc67b33f638492931ea14e57
ammartica <amisadaymartinez@gmail.com>
2021-11-06 15:30:07 +0000

    updated test files

--- Test/basic_print.c
+++ Test/basic_print.c
@@ -1,3 +1,30 @@
+#include <limits.h>
 #include <stdio.h>
+#include "main.h"
 
 
+
+int _printf(const char *format, ...);
+{
+	int i = 0, count = 0;
+
+
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int len;
+	int len2;
+	unsigned int ui;
+	void *addr;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+
+	return (0);
+}
---
+++ Test/print_null.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main(void)
+{
+	printf(NULL);
+	return (0);
+}
