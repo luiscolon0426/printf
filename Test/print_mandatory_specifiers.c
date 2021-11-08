4eae9d22a3f94b29fe2be99b7adc029a4e82dbc7
luiscolon0426 <luiscolon0426@gmail.com>
2021-11-08 20:47:28 +0000

    edited print num function

--- Test/print_mandatory_specifiers.c
+++ Test/print_mandatory_specifiers.c
@@ -55,3 +55,75 @@ int print_num(va_list i)
 	d = d / 10;
 	_putchar(d);
 }*/
+
+
+
+
+/*int print_num(va_list i)
+{
+	int number = 0;
+	int index = 0;
+	int counter = 0;
+	int array[20];
+	int n;
+	{
+		number = va_arg(i, int);
+		if (number < 0)
+		{
+			counter += _putchar ('-');
+			n = number - (-1);
+		}
+		else
+			n = number;
+		if (n == 0)
+		{
+			counter += _putchar (n + 0);
+			return (counter);
+		}
+		while (n != 0)
+		{
+			array[index] = n % 10;
+			n = n / 10;
+			index++;
+		}
+		for (index = index - 1; index >= 0; index--)
+		{
+			counter += _putchar(array[index] + '0');
+		}
+		return (counter);
+	}
+}*/
+
+//under construction
+int print_num(va_list i)
+{
+    int count;
+    int num = va_arg(i, int);
+
+    //si es negativo, print - and make it positive
+    if (num < 0)
+    {
+        _putchar('-');
+        num = -num;
+    }
+
+    //remueve el last digit y recurre to print all of them
+    if (num / 10)
+        w to pas
+    {
+        count += print_num(num);
+    }
+
+    //print last digit
+    _putchar(num % 10 + '0');
+    count++;
+
+    return (count);
+}
+    /*I found this on the internet
+    //int digit;
+    digit = d % 10;
+    digit = digit + '0';
+    d = d / 10;
+    _putchar(d);
+}*/
