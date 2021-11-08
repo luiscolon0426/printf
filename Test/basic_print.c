9f31fc27465d799e9e897dd2e76b5e68147c5c46
ammartica <amisadaymartinez@gmail.com>
2021-11-08 14:53:46 +0000

    modified loops

--- Test/basic_print.c
+++ Test/basic_print.c
@@ -1,69 +1,52 @@
-#include <main.h>
+#include "main.h"
 
-int (*get_specifier(const char format))(va_list)
+int get_specifier(char char_mod, va_list valist)
 {
-	int i = 0;
+	int i = 0, count = 0;
 
 	pt p[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"d", print_decimal},
-		{"i", print_int},
-		{NULL, NULL}
+		{'c', print_char},
+		{'s', print_str},
+//		{'d', print_decimal},
+//		{'i', print_int},
+		{'\0', NULL}
 	};
 
 	while (p[i].t != '\0')
 	{
-		if (p[i].t == format)
-			break;
-
+		if (p[i].t == char_mod)
+		{
+			count += p[i].f(valist);
+			return (count);
+		}
 		i++;
 	}
-	return (p[i].f);
+	return (count);
 }
 
 
 int _printf(const char *format, ...)
 {
 	int i = 0, count = 0;
-	int (*f)(va_list);
 	va_list valist;
 
 	va_start(valist, format);
 
 
-
-	//si el string llegó vacio, end the program and return 0
-	if (format == '\0')
+	if (format == NULL)
 		return (0);
 
-
-
-	//print string cuando no tiene data specifiers
 	while (format[i] != '\0')
 	{
-		if (format[i] == %)
+		if (format[i] == '%')
 		{
-			f = get_specifier(format[i + 1]);
+			count += get_specifier(format[i + 1], valist);
+			return (count);
 		}
-			_putchar(format[i]);
-			count++;
-			i++;
-		}
-	//si string no tenia data specifiers, ya lo imprimí en el ciclo anterior, asi que acaba mi programa y
-	//devuelve el total of chars printed
-	if (!format[i])
-		return (count);
-
-
-
-	//si el character en mi string es un %, busca a cuál data type pertenece el char que le sigue a %
-	if (format[i] == %)
-	{
-		f = get_specifier(format[i + 1]);
-
-
-
+		_putchar(format[i]);
+		count++;
+		i++;
+	}
 	return (count);
 }
 
@@ -76,13 +59,21 @@ int _printf(const char *format, ...)
  */
 int main(void)
 {
-	int len;
-	int len2;
+	int len, len2;
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 
 	printf("This is len: %d and this is len2 %d\n", len, len2);
 
+	len = _printf("print char %c\n", 'l');
+	len2 = printf("print char %c\n", 'l');
+
+	printf("This is len: %d and this is len2 %d\n", len, len2);
+	
+	len = _printf("print string %s\n", "heeey");
+	len2 = printf("print string %s\n", "heeey");
+
+	printf("This is len: %d and this is len2 %d\n", len, len2);
 	return (0);
 }
