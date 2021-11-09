ffee0d7aca64106564624ac7805b347276c20a3f
luiscolon0426 <luiscolon0426@gmail.com>
2021-11-09 13:52:46 +0000

    rename file

--- _printf.c
+++ _printf.c
@@ -22,6 +22,8 @@ int get_specifier(char char_mod, va_list valist)
 	pt p[] = {
 		{'c', print_char},
 		{'s', print_str},
+		{'d', print_num},
+		{'i', print_num},
 		{'\0', NULL}
 	};
 
@@ -73,3 +75,36 @@ int _printf(const char *format, ...)
 	return (count);
 }
 
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+/*int main(void)
+{
+	int len, len2;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+
+	printf("This is len: %d and this is len2 %d\n", len, len2);
+
+	len = _printf("print char %c\n", 'l');
+	len2 = printf("print char %c\n", 'l');
+
+	printf("This is len: %d and this is len2 %d\n", len, len2);
+	
+	len = _printf("print string %s\n", "heeey");
+	len2 = printf("print string %s\n", "heeey");
+
+	printf("This is len: %d and this is len2 %d\n", len, len2);
+
+	len = _printf("print int [%i] [%d]\n", 123467, -123456);
+	len2 = printf("print int [%i] [%d]\n", 123467, -123456);
+
+	printf("This is len: %d and this is len2 %d\n", len, len2);
+
+	return (0);
+}*/
